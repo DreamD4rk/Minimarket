f7a4f0991cbfeb0f37bc0538098f5afb6bdbc949
Miguel <62895464+usr-xyz@users.noreply.github.com>
2022-11-17 17:47:48 -0500

    dame acceso prro :V

---
+++ src/main/java/tienda/com/model/DetalleVenta.java
@@ -0,0 +1,22 @@
+package tienda.com.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "detalles_ventas")
+@Data
+public class DetalleVenta {
+    @Id
+    @OneToOne
+    @JoinColumn(name = "venta_id", referencedColumnName = "id", nullable = false)
+    private Venta venta;
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "producto_id", referencedColumnName = "id", nullable = false)
+    private Producto producto;
+    private Integer cantidad;
+    private Double subtotal;
+}
+
